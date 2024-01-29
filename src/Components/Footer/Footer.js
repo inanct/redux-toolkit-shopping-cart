2749e6e0a18d95d8f5f827d54c5a0f751116a063
inanct <inanc.tekinoktay@gmail.com>
2024-01-29 23:58:52 +0300

    topnavbar

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+        <div className="text-center bg-dark text-white py-2 fixed-bottom">
+            
+        </div>
+    </Fragment>
+  )
+}
+
+export default Footer
