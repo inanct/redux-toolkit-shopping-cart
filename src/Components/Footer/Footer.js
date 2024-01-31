1d8f98e0f1f860d251d45a170fb041d70171bbd7
inanct <inanc.tekinoktay@gmail.com>
2024-01-31 22:44:07 +0300

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <Fragment>
         <div className="text-center bg-dark text-white py-2 fixed-bottom">
+          <span>Copyright & Developed By <a href="https://www.shopier.com/">Shopping</a></span>
             
         </div>
     </Fragment>
