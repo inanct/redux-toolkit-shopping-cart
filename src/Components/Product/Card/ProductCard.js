418395699236e1566d98cdb47a21342b5c2220e6
inanct <inanc.tekinoktay@gmail.com>
2024-02-05 23:49:58 +0300

    product

---
+++ src/Components/Product/Card/ProductCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ".ProductCard.css"
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
+
+const ProductCard = ({product}) => {
+  return (
+    <>
+    <Link to={`product/${product.id}`}>
+    <Card className='custom-card'>
+        <Card.Img variant='top' className='py-3 card-img' src={product.image}/>
+        <Card.Body>
+            <Card.Title className='fs-6'>{product.title.slice(0,30)}</Card.Title>
+            <Card.Text className='text-capitalize text-decoration-none fs-6'>
+             {product.category}
+            </Card.Text>
+        </Card.Body>
+    </Card>
+    </Link>
+    </>
+  )
+}
+
+export default ProductCard
